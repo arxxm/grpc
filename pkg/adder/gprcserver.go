ad7d75060ec7f5cf05f8e9b319ef7e33ba48a35a
arxxm <7767995@gmail.com>
2022-01-23 16:33:53 +0600

    srv

--- pkg/adder/gprcserver.go
+++ pkg/adder/gprcserver.go
@@ -9,6 +9,7 @@ import (
 // GRPCServer ...
 type GRPCServer struct{}
 
+// Add ...
 func (s *GRPCServer) Add(ctx context.Context, req *api.AddRequest) (*api.AddResponse, error) {
-
+	return &api.AddResponse{Result: req.GetX() + req.GetY()}, nil
 }
